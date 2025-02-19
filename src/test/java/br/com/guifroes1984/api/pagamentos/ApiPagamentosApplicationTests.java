ecea21e02ea6d6828cd8dc59f4f05907c9db971c
guifroes1984 <guilhermefroes26@gmail.com>
2025-02-19 17:44:04 -0300

    Start na API.

---
+++ src/test/java/br/com/guifroes1984/api/pagamentos/ApiPagamentosApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.guifroes1984.api.pagamentos;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
