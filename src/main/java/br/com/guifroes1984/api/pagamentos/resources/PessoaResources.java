b4048522b20e3a9b7601a97d4232340d0e8db9c2
guifroes1984 <guilhermefroes26@gmail.com>
2025-02-23 20:55:28 -0300

    Implementando atualizando Pessoa com PUT.

--- src/main/java/br/com/guifroes1984/api/pagamentos/resources/PessoaResources.java
+++ src/main/java/br/com/guifroes1984/api/pagamentos/resources/PessoaResources.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -22,6 +23,7 @@ import br.com.guifroes1984.api.pagamentos.event.RecursoCriadoEvent;
 import br.com.guifroes1984.api.pagamentos.model.Categoria;
 import br.com.guifroes1984.api.pagamentos.model.Pessoa;
 import br.com.guifroes1984.api.pagamentos.repository.PessoaRepository;
+import br.com.guifroes1984.api.pagamentos.service.PessoaService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -32,6 +34,9 @@ public class PessoaResources {
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
+	
+	@Autowired
+	private PessoaService pessoaService;
 	
 	@Autowired
 	private ApplicationEventPublisher publisher;
@@ -59,8 +64,16 @@ public class PessoaResources {
 	
 	@DeleteMapping("/{codigo}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@ApiOperation(value = "Remove uma pessoa pelo código", notes = "Exclui a pessoa do banco de dados pelo código informado.")
 	public void remover(@PathVariable Long codigo) {
 		pessoaRepository.delete(codigo);
+	}
+	
+	@PutMapping("/{codigo}")
+	@ApiOperation(value = "Atualiza uma pessoa pelo código", notes = "Atualiza os dados de uma pessoa no banco de dados.")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
+		Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
+		return ResponseEntity.ok(pessoaSalva);
 	}
 
 }
---
+++ src/main/java/br/com/guifroes1984/api/pagamentos/service/PessoaService.java
@@ -0,0 +1,26 @@
+package br.com.guifroes1984.api.pagamentos.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import br.com.guifroes1984.api.pagamentos.model.Pessoa;
+import br.com.guifroes1984.api.pagamentos.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+		Pessoa pessoaSalva = pessoaRepository.findOne(codigo);
+		if (pessoaSalva == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		return pessoaRepository.save(pessoaSalva);
+	}
+
+}
