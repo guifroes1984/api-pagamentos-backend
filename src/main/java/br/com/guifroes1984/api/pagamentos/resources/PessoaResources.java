8ba92071f84d4fd7d16b44b165bcbcb8ff574ff6
guifroes1984 <guilhermefroes26@gmail.com>
2025-02-26 10:09:29 -0300

    Adiciona documentação Swagger ao método atualizarPropriedadeAtivo

--- src/main/java/br/com/guifroes1984/api/pagamentos/resources/PessoaResources.java
+++ src/main/java/br/com/guifroes1984/api/pagamentos/resources/PessoaResources.java
@@ -78,6 +78,7 @@ public class PessoaResources {
 	
 	@PutMapping("/{codigo}/ativo")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@ApiOperation(value = "Atualiza a propriedade 'ativo' de uma pessoa")
 	public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
 		pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
 	}
