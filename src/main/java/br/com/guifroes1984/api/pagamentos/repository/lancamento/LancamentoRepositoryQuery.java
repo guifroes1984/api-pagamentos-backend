73dfe487ba2b13d39f0ffef9ea47050787437748
guifroes1984 <guilhermefroes26@gmail.com>
2025-02-28 23:47:41 -0300

    Implementando a paginação  de Lançamentos.

--- src/main/java/br/com/guifroes1984/api/pagamentos/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/br/com/guifroes1984/api/pagamentos/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,12 +1,13 @@
 package br.com.guifroes1984.api.pagamentos.repository.lancamento;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import br.com.guifroes1984.api.pagamentos.model.Lancamento;
 import br.com.guifroes1984.api.pagamentos.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 	
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 
 }
