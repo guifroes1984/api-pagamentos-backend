0cbe48f4125481d38ebd3c7c4524c8fd71fbd2a4
guifroes1984 <guilhermefroes26@gmail.com>
2025-03-01 23:55:50 -0300

    Implementando segurança com OAuth2 e Password Flow, e configurar Swagger para autenticação OAuth2.

--- src/main/java/br/com/guifroes1984/api/pagamentos/config/SwaggerConfig.java
+++ src/main/java/br/com/guifroes1984/api/pagamentos/config/SwaggerConfig.java
@@ -2,15 +2,17 @@ package br.com.guifroes1984.api.pagamentos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -23,7 +25,10 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("br.com.guifroes1984.api.pagamentos.resources"))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .securitySchemes(Arrays.asList(securityScheme()))
+                .securityContexts(Arrays.asList(securityContext()));
+
     }
 
     private ApiInfo apiInfo() {
@@ -33,5 +38,22 @@ public class SwaggerConfig {
                 .version("1.0.0")
                 .contact(new Contact("Guilherme Henrique Froes", " ", " "))
                 .build();
+    }
+
+    private SecurityScheme securityScheme() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[]{authorizationScope};
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
     }
 }
