53bd4a413f1f9767b5954e01a48fe6bfc3b11aa2
guifroes1984 <guilhermefroes26@gmail.com>
2025-03-05 23:11:27 -0300

    Adicionando permissões de acesso.

--- src/main/java/br/com/guifroes1984/api/pagamentos/resources/CategoriaResource.java
+++ src/main/java/br/com/guifroes1984/api/pagamentos/resources/CategoriaResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,12 +36,14 @@ public class CategoriaResource {
 
 	@GetMapping
 	@ApiOperation(value = "Endpoint para listar todas as categorias", response = List.class)
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_CATEGORIA') and #oauth2.hasScope('read')")
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
 	}
 
 	@PostMapping
 	@ApiOperation(value = "Endpoint para adicionar uma nova categoria")
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_CATEGORIA') and #oauth2.hasScope('write')")
 	public ResponseEntity<Categoria> adicionar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
@@ -49,6 +52,7 @@ public class CategoriaResource {
 
 	@GetMapping("/{codigo}")
 	@ApiOperation(value = "Endpoint para buscar uma categoria pelo código", response = Categoria.class)
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_CATEGORIA') and #oauth2.hasScope('read')")
 	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
 		Categoria categoria = categoriaRepository.findOne(codigo);
 		return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
