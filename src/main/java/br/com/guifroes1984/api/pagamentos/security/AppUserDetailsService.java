e530ea333af159a35515ffc6997c2a0d691ff5df
guifroes1984 <guilhermefroes26@gmail.com>
2025-03-11 00:18:30 -0300

    Implementando o nome do usuario no token JWT.

--- src/main/java/br/com/guifroes1984/api/pagamentos/security/AppUserDetailsService.java
+++ src/main/java/br/com/guifroes1984/api/pagamentos/security/AppUserDetailsService.java
@@ -8,7 +8,6 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,6 +15,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.guifroes1984.api.pagamentos.model.Usuario;
 import br.com.guifroes1984.api.pagamentos.repository.UsuarioRepository;
+import br.com.guifroes1984.api.pagamentos.security.util.UsuarioSistema;
 
 @Service
 public class AppUserDetailsService implements UserDetailsService {
@@ -27,7 +27,7 @@ public class AppUserDetailsService implements UserDetailsService {
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		Optional<Usuario> usuarioOtional = usuarioRepository.findByEmail(email);
 		Usuario usuario = usuarioOtional.orElseThrow(() -> new UsernameNotFoundException("Usuario e/ou senha incorretas"));
-		return new User(email, usuario.getSenha(), getPermissoes(usuario));
+		return new UsuarioSistema(usuario, getPermissoes(usuario));
 	}
 
 	private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
