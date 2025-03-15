1692947dff3f24d2f243923346f4258de851b4c1
guifroes1984 <guilhermefroes26@gmail.com>
2025-03-14 23:49:13 -0300

    Implementando a atualização de Lançamento.

--- src/main/java/br/com/guifroes1984/api/pagamentos/service/LancamentoService.java
+++ src/main/java/br/com/guifroes1984/api/pagamentos/service/LancamentoService.java
@@ -1,5 +1,6 @@
 package br.com.guifroes1984.api.pagamentos.service;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,6 +28,34 @@ public class LancamentoService {
 		return lancamentoRepository.save(lancamento);
 	}
 	
+	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+		Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+		if (!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+			validarPessoa(lancamento);
+		}
+		
+		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+		
+		return lancamentoRepository.save(lancamento);
+		
+	}
+
+	private void validarPessoa(Lancamento lancamento) {
+		Pessoa pessoa = null;
+		if (lancamento.getPessoa().getCodigo() != null) {
+			pessoa = pessoaRepository.findOne(lancamento.getPessoa().getCodigo());
+		}
+		if (pessoa == null || pessoa.isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+	}
 	
+	private Lancamento buscarLancamentoExistente(Long codigo) {
+		Lancamento lancamentoSalvo = lancamentoRepository.findOne(codigo);
+		if (lancamentoSalvo == null) {
+			throw new IllegalArgumentException();
+		}
+		return lancamentoSalvo;
+	}
 
 }
