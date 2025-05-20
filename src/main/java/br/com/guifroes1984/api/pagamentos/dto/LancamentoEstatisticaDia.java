758ac15e5afcf4297180f6df744856ecb5583d9e
guifroes1984 <guilhermefroes26@gmail.com>
2025-05-19 23:26:18 -0300

    Criando consulta para dados por categoria.

--- src/main/java/br/com/guifroes1984/api/pagamentos/dto/LancamentoEstatisticaDia.java
+++ src/main/java/br/com/guifroes1984/api/pagamentos/dto/LancamentoEstatisticaDia.java
@@ -1,5 +1,46 @@
 package br.com.guifroes1984.api.pagamentos.dto;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import br.com.guifroes1984.api.pagamentos.model.TipoLancamento;
+
 public class LancamentoEstatisticaDia {
+
+	private TipoLancamento tipo;
+
+	private LocalDate dia;
+
+	private BigDecimal total;
+
+	public LancamentoEstatisticaDia(TipoLancamento tipo, LocalDate dia, BigDecimal total) {
+		this.tipo = tipo;
+		this.dia = dia;
+		this.total = total;
+	}
+
+	public TipoLancamento getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoLancamento tipo) {
+		this.tipo = tipo;
+	}
+
+	public LocalDate getDia() {
+		return dia;
+	}
+
+	public void setDia(LocalDate dia) {
+		this.dia = dia;
+	}
+
+	public BigDecimal getTotal() {
+		return total;
+	}
+
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
 
 }
--- src/main/java/br/com/guifroes1984/api/pagamentos/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/br/com/guifroes1984/api/pagamentos/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,13 +1,19 @@
 package br.com.guifroes1984.api.pagamentos.repository.lancamento;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import br.com.guifroes1984.api.pagamentos.dto.LancamentoEstatisticaCategoria;
 import br.com.guifroes1984.api.pagamentos.model.Lancamento;
 import br.com.guifroes1984.api.pagamentos.repository.filter.LancamentoFilter;
 import br.com.guifroes1984.api.pagamentos.repository.projection.ResumoLancamento;
 
 public interface LancamentoRepositoryQuery {
+	
+	public List<LancamentoEstatisticaCategoria> porCategoria(LocalDate mesReferencia);
 	
 	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 	
