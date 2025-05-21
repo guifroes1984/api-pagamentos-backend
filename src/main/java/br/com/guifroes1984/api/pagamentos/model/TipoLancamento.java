5177cdb0802b93945c65be14b3b532cfcecfd737
guifroes1984 <guilhermefroes26@gmail.com>
2025-05-20 22:24:33 -0300

    Criando os campos e parametros do relatório.

--- src/main/java/br/com/guifroes1984/api/pagamentos/model/TipoLancamento.java
+++ src/main/java/br/com/guifroes1984/api/pagamentos/model/TipoLancamento.java
@@ -1,8 +1,18 @@
 package br.com.guifroes1984.api.pagamentos.model;
 
 public enum TipoLancamento {
-	
-	RECEITA,
-	DESPESA
+
+	RECEITA("Receita"), 
+	DESPESA("Despesa");
+
+	private final String descricao;
+
+	TipoLancamento(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
 
 }
