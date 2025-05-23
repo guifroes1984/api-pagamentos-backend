87abd12b34bfaab67d4ed021bb7606b69f61055e
guifroes1984 <guilhermefroes26@gmail.com>
2025-05-23 19:15:03 -0300

    Processando o template e enviando o e-mail.

--- src/main/java/br/com/guifroes1984/api/pagamentos/mail/Mailer.java
+++ src/main/java/br/com/guifroes1984/api/pagamentos/mail/Mailer.java
@@ -1,17 +1,18 @@
 package br.com.guifroes1984.api.pagamentos.mail;
 
-//import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.context.event.ApplicationReadyEvent;
-//import org.springframework.context.event.EventListener;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Component
 public class Mailer {
@@ -19,13 +20,38 @@ public class Mailer {
 	@Autowired
 	private JavaMailSender mailSender;
 	
+	@Autowired
+	private TemplateEngine thymeleaf;
+	
+//	@Autowired
+//	private LancamentoRepository repo;
 //	@EventListener
 //	private void teste(ApplicationReadyEvent event) {
+//		String template = "mail/aviso-lancamentos-vencidos";
+//		
+//		List<Lancamento> lista = repo.findAll();
+//		
+//		Map<String, Object> variaveis = new HashMap<>();
+//		variaveis.put("lancamentos", lista);
+//		
 //		this.enviarEmail("guilhermefroestestedeemail@gmail.com", 
 //				Arrays.asList("guilhermefroes26@gmail.com"), 
-//				"Testando", "Olá!<br/>Teste ok.");
+//				"Testando", template, variaveis);
 //		System.out.println("Terminado o envio de e-mail.");
 //	}
+	
+	public void enviarEmail(String remetente, 
+			List<String> destinatarios, String assunto, String template, 
+			Map<String, Object> variaveis) {
+		Context context = new Context(new Locale("pt", "BR"));
+		
+		variaveis.entrySet().forEach(
+				e -> context.setVariable(e.getKey(), e.getValue()));
+		
+		String mensagem = thymeleaf.process(template, context);
+		
+		this.enviarEmail(remetente, destinatarios, assunto, mensagem);
+	}
 	
 	public void enviarEmail(String remetente, 
 			List<String> destinatarios, String assunto, String mensagem) {
