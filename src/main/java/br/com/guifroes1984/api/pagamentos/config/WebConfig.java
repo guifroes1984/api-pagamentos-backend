c0e992db5f7c7c028a589ceed729e22e7744680d
guifroes1984 <guilhermefroes26@gmail.com>
2025-05-23 10:55:42 -0300

    Criando um agendamento de tarefas(Scheduler).

---
+++ src/main/java/br/com/guifroes1984/api/pagamentos/config/WebConfig.java
@@ -0,0 +1,10 @@
+package br.com.guifroes1984.api.pagamentos.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class WebConfig {
+
+}
--- src/main/java/br/com/guifroes1984/api/pagamentos/service/LancamentoService.java
+++ src/main/java/br/com/guifroes1984/api/pagamentos/service/LancamentoService.java
@@ -10,6 +10,7 @@ import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import br.com.guifroes1984.api.pagamentos.dto.LancamentoEstatisticaPessoa;
@@ -31,6 +32,11 @@ public class LancamentoService {
 	
 	@Autowired 
 	private LancamentoRepository lancamentoRepository;
+	
+	// @Scheduled(cron = "0 0 6 * * *") /*Metodo de agendamento para execução. No caso aqui está todos os dia a 6h da manhã.*/
+	public void avisarSobreLancamentosVencidos() {
+		System.out.println(">>>>>>>>>>>>>>>> Método sendo executado...<<<<<<<<<<<<<<<<");
+	}
 	
 	public byte[] relatorioPorPessoa(LocalDate inicio, LocalDate fim) throws Exception {
 		List<LancamentoEstatisticaPessoa> dados = lancamentoRepository.porPessoa(inicio, fim);
