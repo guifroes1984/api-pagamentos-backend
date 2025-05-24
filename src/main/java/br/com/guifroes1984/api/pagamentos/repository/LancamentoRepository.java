2f3dd669c24b87cea32450c698dd594956154a55
guifroes1984 <guilhermefroes26@gmail.com>
2025-05-24 15:09:24 -0300

    Buscando lançamentos vencidos com Spring Data JPA.

--- src/main/java/br/com/guifroes1984/api/pagamentos/repository/LancamentoRepository.java
+++ src/main/java/br/com/guifroes1984/api/pagamentos/repository/LancamentoRepository.java
@@ -1,10 +1,15 @@
 package br.com.guifroes1984.api.pagamentos.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.guifroes1984.api.pagamentos.model.Lancamento;
 import br.com.guifroes1984.api.pagamentos.repository.lancamento.LancamentoRepositoryQuery;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery {
+	
+	List<Lancamento> findByDataVencimentoLessThanEqualAndDataPagamentoIsNull(LocalDate data);
 
 }
