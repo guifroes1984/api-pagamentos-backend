02e7deaa0ce9c4973c728d7343bce9a444051e93
guifroes1984 <guilhermefroes26@gmail.com>
2025-06-29 20:28:15 -0300

    Configurar autenticação OAuth2 no Swagger UI.

--- src/main/java/br/com/guifroes1984/api/pagamentos/config/SwaggerConfig.java
+++ src/main/java/br/com/guifroes1984/api/pagamentos/config/SwaggerConfig.java
@@ -1,59 +1,62 @@
 package br.com.guifroes1984.api.pagamentos.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.*;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.GrantType;
+import springfox.documentation.service.OAuth;
+import springfox.documentation.service.ResourceOwnerPasswordCredentialsGrant;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.guifroes1984.api.pagamentos.resources"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo())
-                .securitySchemes(Arrays.asList(securityScheme()))
-                .securityContexts(Arrays.asList(securityContext()));
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.guifroes1984.api.pagamentos.resources"))
+				.paths(PathSelectors.any()).build().apiInfo(apiInfo()).securitySchemes(Arrays.asList(securityScheme()))
+				.securityContexts(Arrays.asList(securityContext()));
 
-    }
+	}
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("API - Pagamentos e Gastos Pessoais.")
-                .description("API para pagamentos e gastos pessoais.")
-                .version("1.0.0")
-                .contact(new Contact("Guilherme Henrique Froes", " ", " "))
-                .build();
-    }
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("API - Pagamentos e Gastos Pessoais.")
+				.description("API para pagamentos e gastos pessoais.").version("1.0.0")
+				.contact(new Contact("Guilherme Henrique Froes", " ", " ")).build();
+	}
 
-    private SecurityScheme securityScheme() {
-        return new ApiKey("JWT", "Authorization", "header");
-    }
+	private SecurityScheme securityScheme() {
+		GrantType grantType = new ResourceOwnerPasswordCredentialsGrant("http://localhost:8080/oauth/token");
 
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-                .securityReferences(defaultAuth())
-                .forPaths(PathSelectors.any())
-                .build();
-    }
+		return new OAuth("oauth-security", scopes(), Arrays.asList(grantType));
+	}
 
-    private List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[]{authorizationScope};
-        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
-    }
+	private List<AuthorizationScope> scopes() {
+		return Arrays.asList(new AuthorizationScope("read", "Acesso de leitura"),
+				new AuthorizationScope("write", "Acesso de escrita"));
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder()
+				.securityReferences(Arrays
+						.asList(new SecurityReference("oauth-security", scopes().toArray(new AuthorizationScope[0]))))
+				.forPaths(PathSelectors.any()).build();
+	}
+
 }
