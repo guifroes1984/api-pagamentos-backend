605a2170f304b514f1c099009d97f08d889d211d
guifroes1984 <guilhermefroes26@gmail.com>
2025-07-01 00:48:40 -0300

    Modificações para o Spring Security 5.

--- src/main/java/br/com/guifroes1984/api/pagamentos/config/AuthorizationServerConfig.java
+++ src/main/java/br/com/guifroes1984/api/pagamentos/config/AuthorizationServerConfig.java
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
@@ -21,24 +21,26 @@ import br.com.guifroes1984.api.pagamentos.token.CustomTokenEnhancer;
 
 @Profile("oauth-security")
 @Configuration
-@EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
 				.withClient("angular")
-				.secret("@ngul@r0")
+				.secret("$2a$10$PDnYZeSxylSqwo7feoKGIecnRWOdLs3CQAZ1VmTI8ryC4PrIpoglq") //@ngul@r0
 				.scopes("read", "write")
 				.authorizedGrantTypes("password", "refresh_token")
 				.accessTokenValiditySeconds(1800) // 5
 				.refreshTokenValiditySeconds(3600 * 24) // 10
 			.and()
 				.withClient("mobile")
-				.secret("m0b1l30")
+				.secret("$2a$10$eVJsT1dnmsvVqljVZEWDh.OsCB032B9tNbV4aVxD74qEZRLQLJIrO") //m0b1l30
 				.scopes("read")
 				.authorizedGrantTypes("password", "refresh_token")
 				.accessTokenValiditySeconds(1800)
@@ -54,6 +56,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.tokenStore(tokenStore())
 			.tokenEnhancer(tokenEnhancerChain)
 			.reuseRefreshTokens(false)
+			.userDetailsService(userDetailsService)
 			.authenticationManager(authenticationManager);
 	}
 	
