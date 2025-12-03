74c0adfa702e8f65d6373b56c3de141175390dd3
guifroes1984 <guilhermefroes26@gmail.com>
2025-12-03 00:40:23 -0300

    feat(categorias): implementa CRUD completo de categorias no back-end.

--- src/main/java/br/com/guifroes1984/api/pagamentos/resources/CategoriaResource.java
+++ src/main/java/br/com/guifroes1984/api/pagamentos/resources/CategoriaResource.java
@@ -11,11 +11,14 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.guifroes1984.api.pagamentos.event.RecursoCriadoEvent;
@@ -31,7 +34,7 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	private ApplicationEventPublisher publisher;
 
@@ -57,6 +60,31 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
 		Optional<Categoria> categoria = categoriaRepository.findById(codigo);
 		return categoria.isPresent() ? ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
+	}
+
+	@PutMapping("/{codigo}")
+	@ApiOperation(value = "Endpoint para atualizar uma categoria")
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_CATEGORIA') and #oauth2.hasScope('write')")
+	public ResponseEntity<Categoria> atualizar(@PathVariable Long codigo, @Valid @RequestBody Categoria categoria) {
+		Optional<Categoria> categoriaOptional = categoriaRepository.findById(codigo);
+
+		if (!categoriaOptional.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+
+		Categoria categoriaSalva = categoriaOptional.get();
+		categoriaSalva.setNome(categoria.getNome());
+		categoriaRepository.save(categoriaSalva);
+
+		return ResponseEntity.ok(categoriaSalva);
+	}
+
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@ApiOperation(value = "Endpoint para remover uma categoria")
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_CATEGORIA') and #oauth2.hasScope('write')")
+	public void remover(@PathVariable Long codigo) {
+		categoriaRepository.deleteById(codigo);
 	}
 
 }
