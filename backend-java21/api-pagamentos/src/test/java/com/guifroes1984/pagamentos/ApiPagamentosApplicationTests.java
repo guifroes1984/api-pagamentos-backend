c1dd83f527fe072360d35dab9c06ac61662cc2a2
guifroes1984 <guilhermefroes26@gmail.com>
2026-03-24 00:44:06 -0300

    Cria projeto Spring Boot Java 21

---
+++ backend-java21/api-pagamentos/src/test/java/com/guifroes1984/pagamentos/ApiPagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guifroes1984.pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
